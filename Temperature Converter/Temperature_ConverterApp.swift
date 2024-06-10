57929199142bc37594a69c2d6ce1a34193a09e6d
Anthony Candelino <anthcandelino@gmail.com>
2024-06-10 10:59:33 -0600

    Initial Commit

---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by Anthony Candelino on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
